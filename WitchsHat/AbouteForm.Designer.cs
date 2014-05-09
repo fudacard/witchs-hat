568c0713b35cf189fc6ae9247512b86b8444c6a5
fudacard <donnairo@outlook.jp>
2014-05-09 19:47:44 +0900

    バージョン番号変更.

--- WitchsHat/AbouteForm.Designer.cs
+++ WitchsHat/AbouteForm.Designer.cs
@@ -44,7 +44,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Witch\'s Hat v0.1.0";
+            this.label1.Text = "Witch\'s Hat v0.2.0";
             // 
             // pictureBox1
             // 
